110469dbe926105fffa18185edc98337df5f1e06
vplauzon <vincent_lauzon@yahoo.com.au>
2025-05-03 00:56:19 -0400

    .

---
+++ code/QueryPlan/Planning/Plan.cs
@@ -0,0 +1,19 @@
+﻿using QueryPlan.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryPlan.Planning
+{
+    internal record Plan
+    {
+        public static Plan CreatePlan(Script script)
+        {
+            script.StaticAnalysis();
+
+            throw new NotImplementedException();
+        }
+    }
+}
