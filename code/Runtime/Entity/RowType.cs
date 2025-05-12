6e8080ea3907345f9c76e12bde49de51aa1cd7d1
vplauzon <vincent_lauzon@yahoo.com.au>
2025-05-12 16:41:10 -0400

    .

---
+++ code/Runtime/Entity/RowType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runtime.Entity
+{
+    internal enum RowType
+    {
+        FileVersion,
+        ProcedureRun,
+        ProcedureRunStep
+    }
+}
