0c1333cdf9ef6006e8a296dc5c64712fc2e3638b
vplauzon <vincent_lauzon@yahoo.com.au>
2025-05-12 14:45:33 -0400

    .

---
+++ code/Storage/AzureBlobAppendStorage.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Storage.Blobs.Specialized;
+using Polly;
+
+namespace Storage
+{
+    internal class AzureBlobAppendStorage : IAppendStorage
+    {
+        private readonly AppendBlobClient _appendBlobClient;
+        private readonly AsyncPolicy _writeBlockRetryPolicy;
+        private int _writeCount = 0;
+
+        public AzureBlobAppendStorage(
+            AppendBlobClient blobClient,
+            AsyncPolicy writeBlockRetryPolicy)
+        {
+            _appendBlobClient = blobClient;
+            _writeBlockRetryPolicy = writeBlockRetryPolicy;
+        }
+
+        int IAppendStorage.MaxBufferSize => _appendBlobClient.AppendBlobMaxAppendBlockBytes;
+
+        async Task<bool> IAppendStorage.AtomicAppendAsync(
+            IEnumerable<byte> content,
+            CancellationToken ct)
+        {
+            if (_writeCount == _appendBlobClient.AppendBlobMaxAppendBlockBytes)
+            {
+                return false;
+            }
+            else
+            {
+                await _writeBlockRetryPolicy.ExecuteAsync(async () =>
+                {
+                    if (_writeCount == 0)
+                    {
+                        await _appendBlobClient.CreateAsync();
+                    }
+                    using (var stream = new MemoryStream(content.ToArray()))
+                    {
+                        await _appendBlobClient.AppendBlockAsync(stream, null, ct);
+                        ++_writeCount;
+                    }
+                });
+
+                return true;
+            }
+        }
+    }
+}
