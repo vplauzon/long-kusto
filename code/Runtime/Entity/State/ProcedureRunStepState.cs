7087f5d3eab5e172857c10e3f7c10ca102d6f71a
vplauzon <vincent_lauzon@yahoo.com.au>
2025-05-14 15:20:40 -0400

    .

---
+++ code/Runtime/Entity/State/ProcedureRunStepState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runtime.Entity.State
+{
+    internal enum ProcedureRunStepState
+    {
+        Active,
+        Completed
+    }
+}
